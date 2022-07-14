a24d95ac9b60d5774ad8194624a39e069c073c7d
Carlos <chnava@me.com>
2022-07-14 13:21:51 -0500

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const morgan = require("morgan")
+const postBank= require('./postBank')
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!!!"));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list()
+
+  const html = `<!DOCTYPE html> 
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `<li>${post.title} ${post.name}</li>`)}
+    </ul>
+  </body>
+  </html>`;
+
+res.send(html)
+});
+
+
+
+
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-
-
-//Hello Preston
-//Hello Preston
-//Hello Preston
