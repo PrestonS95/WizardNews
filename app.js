4ddc8f4640f4c178427de948826b4d5980131bfb
PrestonS95 <prestonsullins95@gmail.com>
2022-07-14 12:43:31 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!!!"));
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
+
+
+//Hello Preston
+//Hello Preston
+//Hello Preston
