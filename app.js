11ebe9022759159a19bc3e5ced8751bbbc378d99
Carlos <chnava@me.com>
2022-07-17 15:12:29 -0500

    added error

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const morgan = require("morgan")
-const postBank= require('./postBank')
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
-  const posts = postBank.list()
+  const posts = postBank.list();
 
   const html = `<!DOCTYPE html>
   <html>
@@ -19,8 +18,10 @@ app.get("/", (req, res) => {
   <body>
     <div class="news-list">
       <header><img src="/logo.png"/>Wizard News</header>
-      ${posts.map((post) => 
-        `
+      ${posts
+        .map(
+          (post) =>
+            `
         <div class='news-item'>
           <p>
            <a href="/posts/${post.id}"<span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
@@ -36,36 +37,76 @@ app.get("/", (req, res) => {
   </body>
 </html>`;
 
-res.send(html)
+  res.send(html);
 });
 app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.title}</span>
-            <small>(by ${post.name})</small>
-          </p>
-          <p>${post.content}</p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${post.date}
-          </small>
-        </div>
-    </div>
-  </body>
-</html>`);
+  ////////////////////////////////
+  try {
+    if (!post.id) {
+      throw new Error();
+    } else {
+      res.send(
+        `<!DOCTYPE html>
+          <html>
+           <head>
+             <title>Wizard News</title>
+             <link rel="stylesheet" href="/style.css" />
+           </head>
+           <body>
+           <div class="news-list">
+           <header><img src="/logo.png"/>Wizard News</header>
+           <div class='news-item'>
+             <p>
+               <span class="news-position"> </span><b>${post.title}</b>
+               <small>(by ${post.name})</small>
+             </p>
+             <p >
+               <b>${post.content}</b>|
+             </p>
+             <p>
+             <a href="/">Back</a>
+             </p>
+           </div>
+       </div>
+     </body>
+   </html>`
+      );
+    }
+  } catch (error) {
+    console.log("error");
+    next(error);
+  }
 });
 
+app.use("*", () => {
+  try {
+    throw new Error();
+  } catch (error) {
+    console.log("error");
+    next(error);
+  }
+});
 
+app.use((err, req, res, next) => {
+  res.status(404).send(
+    `
+  <html>
+    <head>
+      <title>404 Error</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+  <body id='mainBody'>
+  <div id="main">
+    <div class="fof">
+        <h1>Error 404</h1>
+        <a href="/">Back</a>
+    </div>
+  </div>
+  </body>`
+  );
+});
 
 const PORT = 1337;
 
